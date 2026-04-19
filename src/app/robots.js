bd4b5faa56fee64abcebd3a608970d2019cc78ab
Abdul1237700 <abdulrouf0812@gmail.com>
2026-04-19 01:11:35 -0600

    sitemap

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,15 +1,16 @@
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      },
-    ],
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/admin/'] }],
     sitemap: [
       'https://cryptonewstrend.com/sitemap.xml',
-      'https://cryptonewstrend.com/news-sitemap/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-en/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-ur/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-ar/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-de/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-fr/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-ru/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-zh/sitemap.xml',
+      'https://cryptonewstrend.com/news-sitemap-es/sitemap.xml',
       'https://cryptonewstrend.com/events-sitemap/sitemap.xml',
       'https://cryptonewstrend.com/articles-sitemap/sitemap.xml',
       'https://cryptonewstrend.com/ico-sitemap/sitemap.xml',
